9a0df03976ee6e892e880434a725e9117e4cccb8
Nikhil Kaul <nk178@duke.edu>
2020-10-14 15:14:17 -0400

    SQLAlchemy init

--- app.py
+++ app.py
@@ -1,5 +1,28 @@
-from flask import Flask
+#import os
+
+from flask import Flask, request, render_template
+#from flask_migrate import Migrate
+from flask_sqlalchemy import SQLAlchemy
+
 app = Flask(__name__)
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://%s:%s@%s/%s' % (
+    # ARGS.dbuser, ARGS.dbpass, ARGS.dbhost, ARGS.dbname
+    'user', 'example','db','recipes'
+)
+
+# initialize the database connection
+db = SQLAlchemy(app)
+
+# initialize database migration management
+#MIGRATE = Migrate(APP, DB)
+
+#from models import *
+
+class User(db.Model): 
+	id = db.Column(db.Integer, primary_key = True)
+	name = db.Column(db.String(100))
 
 @app.route('/')
 def hello_world():
