514dc47b44fc2cecc85a75385fcf80b1f0df2ca5
Connor <crp47@duke.edu>
2020-10-19 10:13:46 -0400

    Querying

--- app.py
+++ app.py
@@ -25,11 +25,32 @@ class recipes(db.Model):
 	n_steps = db.Column(db.Integer)
 	description = db.Column(db.String(200))
 
+	def __init__(self, id, name, minutes, n_steps, description):
+		self.id = id
+		self.name = name
+		self.n_steps = n_steps
+		self.description=description
+
+
 
 @app.route('/')
 def hello_world():
 	return render_template("ID.html", query=recipes.query.all())
 
+@app.route('/recipes/<id>', methods=['GET'])
+def handle_recipe(id):
+    recipe = recipes.query.get_or_404(id)
+
+    if request.method == 'GET':
+        response = {
+            "name": recipe.name,
+            "minutes": recipe.minutes,
+            "n_steps": recipe.n_steps,
+			"description": recipe.description,
+        }
+        return {"message": "success", "recipe": response}
+
 if __name__ == '__main__':
     app.run(debug=True)
+
 
