ae72c42f3994ca16bfc3e17b9c6724f5564f64da
Nicole Patterson <ndp21@duke.edu>
2020-10-20 15:44:13 -0400

    accounts table

--- sql/create.sql
+++ sql/create.sql
@@ -19,3 +19,18 @@ COPY public.recipes(name, id, minutes, n_steps, steps, description, ingredients)
 FROM '/var/lib/postgresql/sql/data.csv'
 DELIMITER ','
 CSV HEADER;
+
+CREATE TABLE public.accounts
+(
+    id bigint NOT NULL,
+    password character varying(15) COLLATE pg_catalog."default" NOT NULL,
+    name character varying(30) COLLATE pg_catalog."default" NOT NULL,
+    minutes smallint,
+    n_steps smallint,
+    CONSTRAINT accounts_pkey PRIMARY KEY (id)
+)
+
+TABLESPACE pg_default;
+
+ALTER TABLE public.accounts
+    OWNER to "user";
