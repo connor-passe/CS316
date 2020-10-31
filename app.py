d055f02c4260e55a2b48827d88e8758f05d55174
Florian Wernthaler <flw5@duke.edu>
2020-10-31 17:50:22 -0400

    Added registration and login pages

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ from flask import request
 from wtform_fields import *
 
 app = Flask(__name__)
+app.secret_key = 'replace later'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://%s:%s@%s/%s' % (
@@ -72,34 +73,16 @@ def parse_list_filter(s):
             x = x.replace(i, '')
         returnList.append(x)
     return returnList
-'''
+
+
 @app.route('/')
 def hello_world():
     return render_template("main.html")
-'''
-@app.route("/", methods=['GET', 'POST'])
+
+@app.route('/signup/', methods=['GET', 'POST'])
 def index():
-
-    reg_form = RegistrationForm()
-
-    # Update database if validation success
-    '''
-    if reg_form.validate_on_submit():
-        username = reg_form.username.data
-        password = reg_form.password.data
-
-        # Hash password
-        hashed_pswd = pbkdf2_sha256.hash(password)
-
-        # Add username & hashed password to DB
-        user = User(username=username, hashed_pswd=hashed_pswd)
-        db.session.add(user)
-        db.session.commit()
-
-        flash('Registered successfully. Please login.', 'success')
-        return redirect(url_for('login'))
-	'''
-    return render_template("index.html", form=reg_form)
+	reg_form = RegistrationForm()
+	return render_template("index.html", form=reg_form)
 
 @app.route('/recipes/', methods=['GET'])
 def handle_recipe():
@@ -125,45 +108,22 @@ def one_recipe(id):
     return render_template("one-recipe.html", query=recipes.query.get(id))
     #goal to show all the info for one recipe
 
-@app.route("/accounts/", methods=['GET', 'POST'])
-def index():
-
-    reg_form = RegistrationForm()
-
-    # Update database if validation success
-    if reg_form.validate_on_submit():
-        username = reg_form.username.data
-        password = reg_form.password.data
-
-        # Hash password
-        hashed_pswd = pbkdf2_sha256.hash(password)
-
-        # Add username & hashed password to DB
-        user = User(username=username, hashed_pswd=hashed_pswd)
-        db.session.add(user)
-        db.session.commit()
-
-        flash('Registered successfully. Please login.', 'success')
-        return redirect(url_for('login'))
-
-    return render_template("index.html", form=reg_form)
-
-
-@app.route("/login/", methods=['GET', 'POST'])
+@app.route('/login/', methods=['GET', 'POST'])
 def login():
 
     login_form = LoginForm()
-
+    '''
     # Allow login if validation success
     if login_form.validate_on_submit():
         user_object = User.query.filter_by(username=login_form.username.data).first()
         login_user(user_object)
         return redirect(url_for('chat'))
+    '''
 
     return render_template("login.html", form=login_form)
 
 
-@app.route("/logout/", methods=['GET'])
+@app.route('/logout/', methods=['GET'])
 def logout():
 
     # Logout user
--- wtform_fields.py
+++ wtform_fields.py
@@ -3,10 +3,12 @@ from wtforms import StringField, PasswordField, SubmitField
 from wtforms.validators import InputRequired, Length, EqualTo, ValidationError
 
 from passlib.hash import pbkdf2_sha256
-from models import User
+#from models import User
 
-
+'''
 def invalid_credentials(form, field):
+    """ Username and password checker """
+
     password = field.data
     username = form.username.data
 
@@ -18,18 +20,26 @@ def invalid_credentials(form, field):
     # Check password in invalid
     elif not pbkdf2_sha256.verify(password, user_data.hashed_pswd):
         raise ValidationError("Username or password is incorrect")
-
+'''
 
 class RegistrationForm(FlaskForm):
+    """ Registration form"""
+
     username = StringField('username', validators=[InputRequired(message="Username required"), Length(min=4, max=25, message="Username must be between 4 and 25 characters")])
     password = PasswordField('password', validators=[InputRequired(message="Password required"), Length(min=4, max=25, message="Password must be between 4 and 25 characters")])
     confirm_pswd = PasswordField('confirm_pswd', validators=[InputRequired(message="Password required"), EqualTo('password', message="Passwords must match")])
 
+    '''
     def validate_username(self, username):
         user_object = User.query.filter_by(username=username.data).first()
         if user_object:
             raise ValidationError("Username already exists. Select a different username.")
+    '''
+
 
 class LoginForm(FlaskForm):
+    """ Login form """
+
     username = StringField('username', validators=[InputRequired(message="Username required")])
-    password = PasswordField('password', validators=[InputRequired(message="Password required"), invalid_credentials])
+    # add back invalid credentials to password later
+    password = PasswordField('password', validators=[InputRequired(message="Password required")])
